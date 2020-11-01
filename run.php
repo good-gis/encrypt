91120ff19ed0725aff94c2cbedc69723d2f4bc21
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-11-01 14:43:48 +0300

    add calc for 2 task

--- run.php
+++ run.php
@@ -6,8 +6,10 @@ use App\Core\CaesarSubstitution;
 
 $caesarCalc = new CaesarSubstitution();
 
-if (isset($argv[1], $argv[2])) {
+if (isset($argv[3])){
+    $caesarCalc->encryptSurnameAthens($argv[1], (int) $argv[2], (int) $argv[3]);
+} else if (isset($argv[1], $argv[2])) {
     $caesarCalc->encryptSurname($argv[1], (int) $argv[2]);
 } else {
-    echo 'Введите фамилию первым параметром (строка), сдвиг вторым параметром (целое число)';
+    echo 'Введите фамилию первым параметром (строка), сдвиг вторым параметром (целое число) - для шифровки подстановкой Цезаря, для шифровки по афинной подстановке Цезаря передайте 2 и 3 параметр числа ключей';
 }
--- src/Core/CaesarSubstitution.php
+++ src/Core/CaesarSubstitution.php
@@ -32,4 +32,30 @@ class CaesarSubstitution
         echo 'Исходный текст' . ' - ' . $surname . PHP_EOL;
         echo 'Зашифрованый текст' . ' - ' . implode('', $encryptArr) . PHP_EOL;
     }
+
+    public function encryptSurnameAthens(string $surname, int $key1, int $key2): void
+    {
+        $surname = mb_strtoupper($surname);
+        $arrSurname = mb_str_split($surname);
+
+        foreach ($arrSurname as $symbol) {
+            $arrNum[] = array_search($symbol, $this->russianAlphabet, true);
+        }
+
+        echo PHP_EOL . 'Общая формула Шифра афинная подстановка Цезаря :'  . PHP_EOL .
+            'Ei=(Ti*A-B)%n, где Ti – порядковый номер буквы исходной последовательности, Еi - порядковый номер соответствующей буквы зашифрованной последовательности, m – размер алфавита (32), ' . PHP_EOL .
+            'А, В – целые числа, причем А и m взаимно простые' . PHP_EOL
+            . "Открытый текст $surname, ключом является пара ($key1, $key2)" . PHP_EOL . PHP_EOL;
+
+        echo 'Используя таблицу с алфавитом, подставим в уравнение значения и получим:' . PHP_EOL;
+
+        foreach ($arrNum as $keyArr => $symbolNum) {
+            $resultNum = (($key1*$symbolNum) + $key2) % 32;
+            $encryptArr[] = $this->russianAlphabet[$resultNum];
+            echo 'E' . $keyArr . '=' . '(' . $key1 . '*'. $symbolNum . '+' . $key2 . ")" . 'mod32' . "=$resultNum" . ' (Буква ' . $this->russianAlphabet[$resultNum] . ')' . PHP_EOL;
+        }
+
+        echo 'Исходный текст' . ' - ' . $surname . PHP_EOL;
+        echo 'Зашифрованый текст' . ' - ' . implode('', $encryptArr) . PHP_EOL;
+    }
 }
